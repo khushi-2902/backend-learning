3cae29dcdb4927cab94dc8b936eb9fa15c55bc0b
khushi-2902 <kg700223@gmail.com>
2026-05-30 11:09:37 +0530

    Added the multer route successfully

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express=require("express");
 const app=express();
 const path=require("path");
 const methodOverride = require('method-override');
+const multer=require("multer");
+const crypto=require("crypto");
+
+
+
 app.use(methodOverride("_method"));
 
 const { v4: uuidv4 } = require('uuid');
@@ -12,11 +17,42 @@ app.set("views",path.join(__dirname,"views"));
 
 app.use(express.urlencoded({extended:true}));
 
+//storing the files in the server using diskStorage
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/images')
+  },
+  filename: function (req, file, cb) {
+     crypto.randomBytes(12,function(error,bytes){
+        const fn=bytes.toString("hex")+path.extname(file.originalname)
+    cb(null, fn)
+  })
+}
+
+})
+
+
+const upload = multer({ storage: storage })
+
 
 app.listen(3000,()=>
 {
     console.log(`app is listening on port ${3000}`);
 });
+
+app.get("/uploads",(req,res)=>
+{
+    
+    res.render("notesUpload.ejs");
+})
+
+app.post("/uploads",upload.single('image'),(req,res)=>
+{
+
+    console.log(req.file);
+     res.send("file upload successfully");
+})
+
 
 let notes=[
     {
@@ -41,6 +77,8 @@ app.get("/notes/new",(req,res)=>
 {
     res.render("notes.ejs");
 });
+
+
 
 //postRoute
 app.post("/notes",(req,res)=>
@@ -101,6 +139,7 @@ app.patch("/notes/:id",(req,res)=>
 // });
 
 
+
 //deleteRoute
 app.delete("/notes/:id",(req,res)=>
 {
@@ -112,7 +151,5 @@ app.delete("/notes/:id",(req,res)=>
 
 
 
-app.post("/upload",(req,res)=>
-{
-    console.log(req.body);
-})
+
+
